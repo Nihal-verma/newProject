77753b31fb8116dea751fea94923c3864c009ea3
Nihal Verma <nihal1999verma@gmail.com>
2024-12-29 17:23:01 +0530

    trtrial change

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server  ${PORT}`);
 });
 
 module.exports = server
